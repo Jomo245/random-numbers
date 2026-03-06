ba1243d51e42d1e7dad4516d9f6b240f1bd2868e
Jomo <johnisige2020@gmail.com>
2026-03-06 07:35:47 +0300

    Updated index.js

--- index.js
+++ index.js
@@ -4,4 +4,5 @@ const result = document.getElementById('result');
 generateBtn.addEventListener('click', function() {
   const randomNumber = Math.floor(Math.random() * 100) + 1;
   result.textContent = `Generated Random Numbers: ${randomNumber}`;
+  
 });
